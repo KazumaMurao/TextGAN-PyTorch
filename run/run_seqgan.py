c25bf45d066c9cc05e873c044b3ff9436343493c
KazumaMurao <kazuma.murao@gmail.com>
2021-03-09 14:47:07 +0900

    for new data

--- run/run_seqgan.py
+++ run/run_seqgan.py
@@ -43,9 +43,9 @@ ADV_train_epoch = 200
 tips = 'SeqGAN experiments'
 
 # ===Oracle  or Real===
-if_real_data = [int(False), int(True), int(True), int(True), int(True), int(True)]
-dataset = ['oracle', 'image_coco', 'emnlp_news', 'chord', 'aozora_wakati', 'musicautobot']
-vocab_size = [5000, 0, 0, 0, 0, 0]
+if_real_data = [int(False), int(True), int(True), int(True), int(True), int(True), int(True)]
+dataset = ['oracle', 'image_coco', 'emnlp_news', 'chord', 'aozora_wakati', 'musicautobot', 'musicautobotdup']
+vocab_size = [5000, 0, 0, 0, 0, 0, 0]
 
 # ===Basic Param===
 data_shuffle = int(False)
